3ea229cf15211be5d82d9988c53d8beca23dd0c4
Stanley Lew <stanley43595@gmail.com>
2024-09-21 10:08:26 -0700

    this is a quick test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center bg-red-500 text-white">
       Hello World
     </div>
   );
