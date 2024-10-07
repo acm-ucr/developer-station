010bb4ed0a0e5d32b2054e5c8fd1a19f70858707
Stanley Lew <stanley43595@gmail.com>
2024-10-07 16:11:26 -0700

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
-    <div className="flex flex-col h-screen w-screen items-center justify-center bg-red-500 text-white">
-      <p className="text-4xl">
-        
-        Hello World</p>
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-red-500 text-white">
+      <p className="text-4xl">Hello World</p>
 
       <p className="font-semibold">Welcome to the Developer Station</p>
     </div>
