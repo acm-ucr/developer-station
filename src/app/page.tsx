2ec73fe4a9df95d9edbe2d8b9426ebd02ece08c1
Stanley Lew <stanley43595@gmail.com>
2024-10-07 16:10:23 -0700

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-red-500 text-white">
-      Hello World
+    <div className="flex flex-col h-screen w-screen items-center justify-center bg-red-500 text-white">
+      <p className="text-4xl">
+        
+        Hello World</p>
+
+      <p className="font-semibold">Welcome to the Developer Station</p>
     </div>
   );
 };
