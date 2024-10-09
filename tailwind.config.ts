8225c876c0b715e191050da01382cd501e7901a3
Daniel <daniellee1807741@gmail.com>
2024-10-08 23:45:50 -0700

    added tailwind custom configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        description: ['25.26px','35.98px']
+      },
+      lineHeight: {
+        185: "185px",
+        87: "87px",
+      },
+      fontFamily: { 
+        Malayalam: ["Malayalam MN", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
