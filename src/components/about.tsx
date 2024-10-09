d7c1b50811b2cfebd14631d409113b0e1f10cf30
Daniel <daniellee1807741@gmail.com>
2024-10-08 23:53:58 -0700

    changed padding

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,8 +6,8 @@ interface AboutProps {
 const About = ({ name, description }: AboutProps) => {
   return (
     <div className="flex flex-col items-center text-center font-Malayalam font-normal">
-      <div className="pb-5 text-7xl">About</div>
-      <div className="pb-36 text-9xl">{name}</div>
+      <div className="text-7xl">About</div>
+      <div className="pb-20 text-9xl">{name}</div>
       <div className="size-4/12 text-center text-description">
         {description}
       </div>
