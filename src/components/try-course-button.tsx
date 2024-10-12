85e0a7fc27bf3a8c5b96e55e7bda984a7cd683db
DM524oigjroigre <131196777+DM524oigjroigre@users.noreply.github.com>
2024-10-12 15:32:59 -0700

    Created a 'TRY COURSE' component

---
+++ src/components/try-course-button.tsx
@@ -0,0 +1,18 @@
+const TryCourseButton = () => {
+  return (
+    <div className="relative inline-block w-[20%] p-1.5">
+      {/* Larger shape beneath */}
+      <div className="absolute inset-0 rounded-full bg-[#cff2aa]"></div>
+
+      {/* Main button */}
+      <div className="flex justify-center">
+        <button className="relative w-[95%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold uppercase text-black shadow-md transition-all duration-300 ease-in-out hover:shadow-lg">
+          <span className="relative z-10">TRY COURSE</span>
+          <div className="absolute inset-0 rounded-full bg-gradient-to-b from-transparent to-[#eaf7dc] opacity-50"></div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TryCourseButton;
