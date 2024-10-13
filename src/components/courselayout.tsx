1f7bf52df2c03246a2b33ca9ca6320f206a61ed3
rattahamberg <walter.singleton.2024@gmail.com>
2024-10-13 12:29:02 -0700

    Formatting

--- src/components/courselayout.tsx
+++ src/components/courselayout.tsx
@@ -5,12 +5,12 @@ interface CourseLayoutProps {
 
 const CourseLayout: React.FC<CourseLayoutProps> = ({ title, description }) => {
   return (
-    <div className="mx-auto flex w-full max-w-[calc(100%-100px)] flex-col items-center rounded-[20px] bg-course-layout-background/80 text-center font-Malayalam font-normal p-8">
-      <div className="text-center font-Malayalam text-[80px] font-normal leading-[100px] pt-8 pb-4">
+    <div className="mx-auto flex w-full max-w-[calc(100%-100px)] flex-col items-center rounded-[20px] bg-course-layout-background/80 p-8 text-center font-Malayalam font-normal">
+      <div className="pb-4 pt-8 text-center font-Malayalam text-[80px] font-normal leading-[100px]">
         {title}
       </div>
 
-      <div className="text-center font-Malayalam text-[25px] font-normal leading-[40px] px-10">
+      <div className="px-10 text-center font-Malayalam text-[25px] font-normal leading-[40px]">
         {description}
       </div>
     </div>
