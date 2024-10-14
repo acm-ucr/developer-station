11da03e45bb3ddd1fa749b6a0dbbcac2e0a68ae3
Carla Hau <61822001+carlahauu@users.noreply.github.com>
2024-10-13 19:44:05 -0700

    Update src/components/CourseName.tsx
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/components/CourseName.tsx
+++ src/components/CourseName.tsx
@@ -4,7 +4,7 @@ interface CourseNameProps {
 
 const CourseName = ({ name }: CourseNameProps) => {
   return (
-    <div className="flex w-full flex-col items-center rounded-2xl bg-dark-cyan text-center font-Malayalam font-normal">
+    <div className="flex w-full flex-col items-center rounded-2xl bg-dark-cyan text-center font-Malayalam">
       <div className="pb-16 pt-16 text-6xl text-white">{name}</div>
     </div>
   );
