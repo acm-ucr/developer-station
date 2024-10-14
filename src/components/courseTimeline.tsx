a1c2da81c9f90344a4408b3ea80beab0e4a0af3d
Mooogy <francismigsanjuan@gmail.com>
2024-10-13 18:19:34 -0700

    Created prototype for Course Timeline component

---
+++ src/components/courseTimeline.tsx
@@ -0,0 +1,60 @@
+const CourseTimeline = () => {
+  return (
+    <div className="mx-auto flex w-full max-w-6xl flex-col items-start justify-start rounded-lg bg-[#003850] px-8 py-12 text-[#FFF4BA]">
+      {/* Title */}
+      <h1 className="mb-8 w-full text-center text-4xl">Timeline of Course</h1>
+
+      {/* First List */}
+      <div className="mb-8">
+        <h2 className="mb-4 text-2xl">Lorem Ipsum</h2>
+        <ul className="ml-7 list-[upper-alpha] space-y-8">
+          <li className="pl-4">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          </li>
+          <li className="pl-4">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          </li>
+          <li className="pl-4">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          </li>
+          <li className="pl-4">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          </li>
+        </ul>
+      </div>
+
+      {/* Second List */}
+      <div className="mb-8">
+        <h2 className="mb-4 text-2xl">Lorem Ipsum</h2>
+        <ul className="ml-7 list-[upper-alpha] space-y-8">
+          <li className="pl-1">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          </li>
+          <li className="pl-1">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CourseTimeline;
