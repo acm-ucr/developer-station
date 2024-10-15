abc7db4ab17f671d3788d002640ec6e7a831948b
Walter Singleton <walter.singleton.2024@gmail.com>
2024-10-15 14:02:47 -0700

    Update src/components/courselayout.tsx
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/components/courselayout.tsx
+++ src/components/courselayout.tsx
@@ -3,7 +3,7 @@ const CourseLayout = () => {
     <div className="bg-transparent-80 p-auto font-Malayalam mx-auto flex w-[95vw] flex-col items-center rounded-[1vw] bg-devstation-green-200 p-[2.5vw] text-center font-normal">
       <div className="text-[4vw]">Our Projects</div>
 
-      <div className="text-[1.15vw]">
+      <div className="text-xl">
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
       </div>
