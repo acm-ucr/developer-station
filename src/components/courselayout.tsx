57fec1d83ab911a7100e0eed772f00f14daf7ad2
Stanley Lew <stanley43595@gmail.com>
2024-10-15 13:08:06 -0700

    format

--- src/components/courselayout.tsx
+++ src/components/courselayout.tsx
@@ -1,6 +1,6 @@
 const CourseLayout = () => {
   return (
-    <div className="bg-transparent-80 p-auto mx-auto flex w-[95vw] flex-col items-center rounded-[1vw] bg-devstation-green-200 p-[2.5vw] text-center font-Malayalam font-normal">
+    <div className="bg-transparent-80 p-auto font-Malayalam mx-auto flex w-[95vw] flex-col items-center rounded-[1vw] bg-devstation-green-200 p-[2.5vw] text-center font-normal">
       <div className="text-[4vw]">Our Projects</div>
 
       <div className="text-[1.15vw]">
