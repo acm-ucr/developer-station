5fa3a78396ca0e17ed9726a4937f6f48bfb83f6e
rattahamberg <walter.singleton.2024@gmail.com>
2024-10-14 22:02:09 -0700

    Simplified/improved CSS styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import CourseLayout from "@/components/courselayout";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-devstation-green-100 text-white">
-      <div className="text-white">The Developer's Station</div>
+      <CourseLayout />
     </div>
   );
 };
--- src/components/courselayout.tsx
+++ src/components/courselayout.tsx
@@ -1,9 +1,9 @@
 const CourseLayout = () => {
   return (
-    <div className="mx-auto flex w-full max-w-5xl flex-col items-center rounded-2xl bg-devstation-green-200/80 p-8 text-center font-Malayalam font-normal">
-      <div className="pb-4 pt-8 text-6xl leading-tight">Our Projects</div>
+    <div className="bg-transparent-80 p-auto mx-auto flex w-[80vw] flex-col items-center rounded-[1vw] bg-devstation-green-200 p-[2vw] text-center font-Malayalam font-normal">
+      <div className="text-[4vw]">Our Projects</div>
 
-      <div className="px-10 text-lg leading-8">
+      <div className="text-[1vw]">
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
       </div>
