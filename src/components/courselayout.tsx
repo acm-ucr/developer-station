5ae08c821a777bcc80e98fe7dc70d820a4312753
rattahamberg <walter.singleton.2024@gmail.com>
2024-10-14 22:36:50 -0700

    Fine touches to the CSS styling

--- src/components/courselayout.tsx
+++ src/components/courselayout.tsx
@@ -1,9 +1,9 @@
 const CourseLayout = () => {
   return (
-    <div className="bg-transparent-80 p-auto mx-auto flex w-[80vw] flex-col items-center rounded-[1vw] bg-devstation-green-200 p-[2vw] text-center font-Malayalam font-normal">
+    <div className="bg-transparent-80 p-auto mx-auto flex w-[95vw] flex-col items-center rounded-[1vw] bg-devstation-green-200 p-[2vw] text-center font-Malayalam font-normal">
       <div className="text-[4vw]">Our Projects</div>
 
-      <div className="text-[1vw]">
+      <div className="text-[1.15vw]">
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
       </div>
