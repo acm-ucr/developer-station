4a39ab9b7e4b9cf7efb2246edd393981caff0fe7
rattahamberg <walter.singleton.2024@gmail.com>
2024-10-14 22:14:08 -0700

    Please please please work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-devstation-green-100 text-white">
-      <CourseLayout />
+      <div className="text-white">The Developer's Station</div>
     </div>
   );
 };
