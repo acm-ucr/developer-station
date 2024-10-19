970232344c45df20eaf2fc4ee75f36110bf524dd
darsh175223 <107499880+darsh175223@users.noreply.github.com>
2024-10-19 16:21:38 -0700

    Update src/components/try-course-button.tsx
    remove the transition
    
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/components/try-course-button.tsx
+++ src/components/try-course-button.tsx
@@ -4,7 +4,7 @@ const TryCourseButton = () => {
       <div className="absolute inset-0 rounded-full bg-[#cff2aa]"></div>
 
       <div className="flex justify-center">
-        <button className="relative w-[96%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold uppercase text-black shadow-md transition-all duration-300 ease-in-out hover:shadow-lg">
+        <button className="relative w-[96%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold text-black shadow-md">
           <span className="relative z-10">TRY COURSE</span>
           <div className="absolute inset-0 rounded-full bg-gradient-to-b from-transparent to-[#eaf7dc] opacity-50"></div>
         </button>
