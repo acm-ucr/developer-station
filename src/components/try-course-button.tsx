462e03a43f8ed5173f71cc76c869a3b45d29cf45
darsh175223 <darshanm2114@gmail.com>
2024-10-19 16:34:14 -0700

    applied new changes to styling

--- src/components/try-course-button.tsx
+++ src/components/try-course-button.tsx
@@ -1,13 +1,10 @@
 const TryCourseButton = () => {
   return (
-    <div className="relative inline-block w-[20%] p-1.5">
-      {/* Larger shape beneath */}
-      <div className="absolute inset-0 rounded-full bg-[#cff2aa]"></div>
+    <div className="relative w-[20%] p-1.5">
 
-      {/* Main button */}
       <div className="flex justify-center">
-        <button className="relative w-[96%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold uppercase text-black shadow-md transition-all duration-300 ease-in-out hover:shadow-lg">
-          <span className="relative z-10">TRY COURSE</span>
+        <button className="relative w-[96%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold text-black shadow-md">
+          <p>TRY COURSE</p>
           <div className="absolute inset-0 rounded-full bg-gradient-to-b from-transparent to-[#eaf7dc] opacity-50"></div>
         </button>
       </div>
