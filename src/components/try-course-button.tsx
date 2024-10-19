b04ef510a4cd2342ac6cb2154685ab5eaffa71ae
darsh175223 <107499880+darsh175223@users.noreply.github.com>
2024-10-19 16:20:33 -0700

    Update src/components/try-course-button.tsx
    remove comment
    
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/components/try-course-button.tsx
+++ src/components/try-course-button.tsx
@@ -4,7 +4,6 @@ const TryCourseButton = () => {
       {/* Larger shape beneath */}
       <div className="absolute inset-0 rounded-full bg-[#cff2aa]"></div>
 
-      {/* Main button */}
       <div className="flex justify-center">
         <button className="relative w-[96%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold uppercase text-black shadow-md transition-all duration-300 ease-in-out hover:shadow-lg">
           <span className="relative z-10">TRY COURSE</span>
