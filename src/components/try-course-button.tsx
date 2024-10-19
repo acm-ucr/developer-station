64ea34b708d626136768fa983f9c1a84b6c9ab82
darsh175223 <107499880+darsh175223@users.noreply.github.com>
2024-10-19 16:26:25 -0700

    Update src/components/try-course-button.tsx
    changing the tag to <p>
    
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/components/try-course-button.tsx
+++ src/components/try-course-button.tsx
@@ -5,7 +5,7 @@ const TryCourseButton = () => {
 
       <div className="flex justify-center">
         <button className="relative w-[96%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold text-black shadow-md">
-          <span className="relative z-10">TRY COURSE</span>
+          <p>TRY COURSE</p>
           <div className="absolute inset-0 rounded-full bg-gradient-to-b from-transparent to-[#eaf7dc] opacity-50"></div>
         </button>
       </div>
