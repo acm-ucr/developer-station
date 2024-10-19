2f6a19f5acdeb143ab9ba415fc38eb0c1b81b6c3
darsh175223 <107499880+darsh175223@users.noreply.github.com>
2024-10-19 16:30:59 -0700

    Update src/components/try-course-button.tsx
    removed div
    
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/components/try-course-button.tsx
+++ src/components/try-course-button.tsx
@@ -6,7 +6,7 @@ const TryCourseButton = () => {
       <div className="flex justify-center">
         <button className="relative w-[96%] rounded-full bg-[#eaf7dc] px-12 py-3 text-xl font-bold text-black shadow-md">
           <p>TRY COURSE</p>
-          <div className="absolute inset-0 rounded-full bg-gradient-to-b from-transparent to-[#eaf7dc] opacity-50"></div>
+        
         </button>
       </div>
     </div>
