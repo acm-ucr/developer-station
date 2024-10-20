8ca8f860c8a296f4d46d234883039c9049d511ba
Siddhant Thakare <siddhantthakare7@outlook.com>
2024-10-20 15:19:00 -0700

    Changed the custom color names for project card component

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -12,13 +12,13 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   title,
 }) => {
   return (
-    <div className="flex flex-col items-center rounded-2xl border-4 border-devstation-project-card-border-green bg-devstation-project-card-bg-green p-5">
+    <div className="flex flex-col items-center rounded-2xl border-4 border-devstation-green-500 bg-devstation-green-400 p-5">
       <div className="mb-2 flex w-3/4 items-end justify-end rounded-2xl">
         {backgroundImage && (
           <Image src={backgroundImage} alt={title} className="rounded-2xl" />
         )}
 
-        <div className="absolute flex items-end justify-end p-[2px_20px] text-right uppercase">
+        <div className="absolute flex items-end justify-end px-[2px] py-[20px] text-right uppercase">
           {title}
         </div>
       </div>
