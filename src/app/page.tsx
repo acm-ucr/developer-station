ee0f26c8bd455a43dadf0e8a703de841d090f1ff
darsh175223 <darshanm2114@gmail.com>
2024-10-19 17:21:33 -0700

    Changed the colors to dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import TryCourseButton from "../components/try-course-button";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-devstation-green-100 text-white">
       <div className="text-white">The Developer's Station</div>
-      <br></br>
-      <TryCourseButton></TryCourseButton>
     </div>
   );
 };
