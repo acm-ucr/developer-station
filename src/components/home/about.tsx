fecaab5ace702a91f4595b031d72dd145d4c8c53
Daniel Lee <daniellee1807741@gmail.com>
2024-10-20 03:28:10 -0700

    use vw, fixed margins

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -1,13 +1,18 @@
 const About = () => {
   return (
-    <div className="font-Malayalam flex flex-col items-center text-center">
-      <div className="text-[4vh]">About</div>
-      <div className="bg-black text-[8vh]">Name</div>
-      <div className="text-description w-5/12 text-center text-[2vh]">
+    <div className="font-Malayalam flex h-screen w-screen flex-col items-center text-center">
+      <div>
+        <div className="text-[2.7vw]">About</div>
+        <div className="-mt-[1.5vw] text-[5vw]">"NAME"</div>
+      </div>
+      <div className="mt-[2vw] w-5/12 text-center text-[1vw]">
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-        Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
       </div>
     </div>
   );
