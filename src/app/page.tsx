7d841af4125092069da1d4b54bde6a1391ad5604
Daniel <daniellee1807741@gmail.com>
2024-10-20 02:29:26 -0700

    use vh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import About from "../components/home/about";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-red-500 text-white">
-      Hello World
+      <About/>
     </div>
   );
 };
---
+++ src/components/home/about.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className="flex flex-col items-center text-center font-Malayalam">
+      <div className="text-[4vh]">About</div>
+      <div className="bg-black text-[8vh]">Name</div>
+      <div className="w-5/12 text-[2vh] text-center text-description">
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      </div>
+    </div>
+  );
+};
+export default About;
