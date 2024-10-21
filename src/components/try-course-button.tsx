cf81e894b81f70d50c6a9cfa707f63098c986908
Stanley Lew <stanley43595@gmail.com>
2024-10-21 11:34:42 -0700

    size switch

--- src/components/try-course-button.tsx
+++ src/components/try-course-button.tsx
@@ -1,7 +1,7 @@
 const TryCourseButton = () => {
   return (
-    <div className="w-1/3 rounded-full bg-devstation-green-300 px-7 py-3">
-      <button className="w-full rounded-full bg-devstation-green-100 px-5 py-1 text-[3vw] text-devstation-yellow-100">
+    <div className="w-1/3 rounded-full bg-devstation-green-300 px-4 py-2">
+      <button className="w-full rounded-full bg-devstation-green-100 px-5 pt-2 text-[3vw] text-devstation-yellow-100">
         TRY COURSE
       </button>
     </div>
