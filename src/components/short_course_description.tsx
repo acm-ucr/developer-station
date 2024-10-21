f7d3551b9b262ec716fb1d7559049b684d3924d6
Stanley Lew <stanley43595@gmail.com>
2024-10-21 12:08:16 -0700

    remove font

--- src/components/short_course_description.tsx
+++ src/components/short_course_description.tsx
@@ -1,6 +1,6 @@
 const ShortCourseDescription = () => {
   return (
-    <div className="font-Malayalam mx-auto flex w-[90vw] flex-col items-center rounded-2xl bg-devstation-cyan-100 p-[5vw] text-center">
+    <div className="mx-auto flex w-[90vw] flex-col items-center rounded-2xl bg-devstation-cyan-100 p-[5vw] text-center">
       <div className="w-[32vw] text-[2vw] text-devstation-yellow-200">
         About Course Area lorem ipsum lorem ipsum orem ipsum lorem ipsum lorem
         ipsum orem ipsum lorem ipsum
