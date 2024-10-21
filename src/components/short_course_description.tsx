ed4a59d5d6e9f08bb8e9e79e3d58de50dc833db7
Kevin Ye <kye019@ucr.edu>
2024-10-21 11:52:38 -0700

    resolved merge conflicts

--- src/components/short_course_description.tsx
+++ src/components/short_course_description.tsx
@@ -1,7 +1,7 @@
 const ShortCourseDescription = () => {
   return (
     <div className="font-Malayalam mx-auto flex w-[90vw] flex-col items-center rounded-2xl bg-devstation-cyan-100 p-[5vw] text-center">
-      <div className="w-[32vw] text-[2vw] text-devstation-yellow-100">
+      <div className="w-[32vw] text-[2vw] text-devstation-yellow-200">
         About Course Area lorem ipsum lorem ipsum orem ipsum lorem ipsum lorem
         ipsum orem ipsum lorem ipsum
       </div>
