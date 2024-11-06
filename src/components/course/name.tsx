735030341eddbe07c362e063118800d5e9bfd55b
Kevin Ye <kye019@ucr.edu>
2024-11-06 00:18:26 -0800

    made corrections

--- src/components/course/name.tsx
+++ src/components/course/name.tsx
@@ -4,7 +4,7 @@ interface CourseNameProps {
 
 const Name = ({ children }: CourseNameProps) => {
   return (
-    <div className="font-Malayalam w-[65vw] rounded-2xl bg-devstation-cyan-100 p-[2vw] text-center text-[4vw] text-white">
+    <div className="font-Malayalam w-3/5 rounded-2xl bg-devstation-cyan-100 p-4 text-center text-[4vw] text-white">
       {children}
     </div>
   );
--- src/components/course/short_course_description.tsx
+++ src/components/course/short_course_description.tsx
@@ -1,6 +1,6 @@
 const ShortCourseDescription = () => {
   return (
-    <div className="mx-auto flex w-[75vw] flex-col items-center rounded-2xl bg-devstation-cyan-100 p-[5vw] text-center">
+    <div className="mx-auto flex w-3/4 flex-col items-center rounded-2xl bg-devstation-cyan-100 p-[5vw] text-center">
       <div className="w-[32vw] text-[2vw] text-devstation-yellow-200">
         About Course Area lorem ipsum lorem ipsum orem ipsum lorem ipsum lorem
         ipsum orem ipsum lorem ipsum
