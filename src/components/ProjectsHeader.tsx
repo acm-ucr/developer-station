825f5678dc75be0656347e967891c0e9d72b49da
Siddhant Thakare <siddhantthakare7@outlook.com>
2024-11-09 22:32:03 -0800

    Added light/dark mode changes in projects header

--- src/components/ProjectsHeader.tsx
+++ src/components/ProjectsHeader.tsx
@@ -1,11 +1,22 @@
-import ProjectPic from "@/public/images/Projectshome1.webp";
+import BgImageDark from "@/public/projects/projects-header-bg-image-dark.webp";
+import BgImageLight from "@/public/projects/projects-header-bg-image-light.webp";
 import Image from "next/image";
 
 const ProjectsHome = () => {
   return (
     <div className="flex">
-      <Image alt="header" src={ProjectPic} />
-      <p className="absolute ml-[38%] mt-[42%] font-malayalam text-[11vw] text-devstation-yellow-300">
+      <Image className="hidden dark:block" alt="header" src={BgImageDark} />
+
+      <Image
+        className="dark:hidden"
+        alt="header"
+        src={BgImageLight}
+        style={{
+          background: "linear-gradient(180deg, #6FCFF7 0%, #00498D 100%)",
+        }}
+      />
+
+      <p className="absolute ml-[38%] mt-[42%] font-malayalam text-[11vw] text-white dark:text-devstation-yellow-300">
         PROJECTS
       </p>
     </div>
